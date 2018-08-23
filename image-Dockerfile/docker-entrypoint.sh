25a41ef5b839af72366aed170a2673181547ee1c
siangyeh8818 <siangyeh@gmail.com>
2018-08-23 20:10:48 +0800

    fix docker-entrypoint.sh

--- image-Dockerfile/docker-entrypoint.sh
+++ image-Dockerfile/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-mkdir -p /12345
-#sleep 2s
+#mkdir -p /12345
+sleep 2s
+echo "run init script"
 #systemctl status glusterd.service 
-#go run /Golang/src/glusterfs-init/main.go
+go run /Golang/src/glusterfs-init/main.go
